ff01ca78f526fce904d2a780c412536049ccaf69
cupp <bcupp@pepperdine.edu>
2014-03-19 00:06:02 -0700

    added myMax

---
+++ myMax/myMax.R
@@ -0,0 +1,10 @@
+#   mySum.R
+#
+#   function myMax(v) returns the largest elements of numeric vector, v.
+#   Use a loop. You are not allowed to use the max function in your solution.
+
+
+myMax <- function(v) {
+  assert(length(v) > 0)
+  
+}
---
+++ myMax/tests/1.R
@@ -0,0 +1,13 @@
+test.examples <- function()
+{
+  checkEquals(6, myMax(c(4,1,2,3,6)))
+  checkEquals(myMax(c(17,5,6,2,4)), 17)
+  checkEquals(myMax(c(8)), 8)
+  checkEquals(myMax(c(-3.2,-2.1,-5.6)), -2.1)
+  checkEquals(mySum(c(-5,2,4)), 4)
+}
+
+test.deactivation <- function()
+{
+  DEACTIVATED('Deactivating this test function')
+}
