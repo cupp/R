c7e27fd47d803946ad6d63d79d1ce1ae64de406e
cupp <cupp@minsky.cslab.pepperdine.edu>
2014-03-18 17:03:13 -0700

    added mySum.R

---
+++ mySum/mySum.R
@@ -0,0 +1,8 @@
+#   mySum.R
+#
+#   function mySum(v) returns the sum of the elements of numeric vector, v.
+#   Use a loop. You are not allowed to use the sum function in your solution.
+
+mySum <- function(v) {
+  
+}
