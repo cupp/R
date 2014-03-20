dfa3bd8eb13bf73c0aa2d634d2891dbfeacb8e3b
cupp <bcupp@pepperdine.edu>
2014-03-19 19:18:36 -0700

    Added project myLength

---
+++ myLength/myLength.R
@@ -0,0 +1,9 @@
+#   mySum.R
+#
+#   function myLength(v) returns the number of elements in vector v.
+#   Use a loop. You are not allowed to use the length function in your solution.
+
+
+myLength <- function(v) {
+
+}
